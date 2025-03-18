a205033775718b8e3886be671f7cccc7437af704
eda3 <gerorin1010@gmail.com>
2025-03-18 22:24:06 +0900

    delete &lt;/span&gt;

--- src/commands/update/framedata_json.rs
+++ src/commands/update/framedata_json.rs
@@ -49,6 +49,7 @@ pub async fn frames_to_json(
         char_page_response_json.replace(r#"&lt;span style=&quot;color: \n#bcbd0c&quot; &gt"#, "");
 
     char_page_response_json = char_page_response_json.replace(r#"&#039;"#, "");
+    char_page_response_json = char_page_response_json.replace(r#"&lt;/span&gt;"#, "");
 
     char_page_response_json =
         char_page_response_json.replace(r#""input":"5L""#, r#""input":"近L""#);
